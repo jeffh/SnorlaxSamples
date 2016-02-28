684b1c84f25dfb6f7241d6feb0fa5cb21a517130
Jeff Hui <jeff@jeffhui.net>
2016-02-27 23:58:50 -0800

    Removed unused targets

--- SPMApp/Package.swift
+++ SPMApp/Package.swift
@@ -2,7 +2,6 @@ import PackageDescription
 
 let package = Package(
     name: "SPMApp",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/jeffh/Snorlax.git", majorVersion: 0, minor: 1)
     ]
